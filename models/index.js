b427255d75674e8a060d259c6de957f775e6e139
Michał Ardziejewski <adjweb@gmail.com>
2018-11-13 14:14:02 +0100

    Remove config.json for Sequelize

--- models/index.js
+++ models/index.js
@@ -5,15 +5,21 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require('../modules/config/config')
 const db = {}
 
-let sequelize
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
+const sequelize = new Sequelize(config.db().database, config.db().user, config.db().password, {
+    host: config.db().host,
+    port: config.db().port,
+    dialect: 'mysql',
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+})
 
 fs.readdirSync(__dirname)
     .filter(file => {
